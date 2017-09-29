45d4e2ec508e8294db1162276c46e9dc2a7eb5b8
Wojciech Johaniuk <wojciech.johaniuk@braintri.com>
2017-09-29 17:03:28 +0200

    added distance method to point class

--- sandbox/src/main/java/qa/kurs/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/kurs/sandbox/MyFirstProgram.java
@@ -1,5 +1,5 @@
 package qa.kurs.sandbox;
-import static java.lang.Math.*;
+
 
 public class MyFirstProgram {
 
@@ -10,13 +10,11 @@ public class MyFirstProgram {
     Point r = new Point(4,6, 5 , 8);
 
     System.out.println("Distance between points x(" + r.x1 + ","+ r.x2+ ") " +
-            "and y(" + r.y1 + "," + r.y2 +") is " + distance(r));
+            "and y(" + r.y1 + "," + r.y2 +") is " + r.distance());
   }
 
 
 
 
-  public static double distance(Point r) {
-    return sqrt((r.x2 - r.x1)^2+(r.y2-r.y1)^2);
-  }
+
 }
--- sandbox/src/main/java/qa/kurs/sandbox/Point.java
+++ sandbox/src/main/java/qa/kurs/sandbox/Point.java
@@ -1,5 +1,7 @@
 package qa.kurs.sandbox;
 
+import static java.lang.Math.sqrt;
+
 public class Point {
 
   public int x1;
@@ -13,5 +15,13 @@ public class Point {
     this.y1 = y1;
     this.y2 = y2;
 
+  }
+
+  public Point2(int x2,int y2){
+
+  }
+
+  public double distance() {
+    return sqrt((this.x2 - this.x1)^2+(this.y2-this.y1)^2);
   }
 }
