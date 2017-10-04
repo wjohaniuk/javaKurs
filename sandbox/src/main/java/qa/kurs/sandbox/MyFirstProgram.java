e8fdb1fb843a2e4aceed10a2aea5b798ff96b63e
Wojciech Johaniuk <wojciech.johaniuk@braintri.com>
2017-10-04 15:09:24 +0200

    home work 2 after fixes

--- sandbox/src/main/java/qa/kurs/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/qa/kurs/sandbox/MyFirstProgram.java
@@ -1,19 +1,24 @@
 package qa.kurs.sandbox;
 
 
+import static java.lang.Math.sqrt;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
 
 
 
-    Point r = new Point(4,6, 5 , 8);
+    Point p1 = new Point(2,5);
+    Point p2 = new Point(6,6);
 
-    System.out.println("Distance between points x(" + r.x1 + ","+ r.x2+ ") " +
-            "and y(" + r.y1 + "," + r.y2 +") is " + r.distance());
+    System.out.println("Distance between points x(" + p1.x + ","+ p2.x+ ") " +
+            "and y(" + p1.y + "," + p2.y +") is " + distance(p1, p2));
   }
 
-
+    public static double distance(Point p1, Point p2) {
+    return sqrt((p2.x - p1.x) ^ 2 + (p2.y - p1.y) ^ 2);
+  }
 
 
 
--- sandbox/src/main/java/qa/kurs/sandbox/Point.java
+++ sandbox/src/main/java/qa/kurs/sandbox/Point.java
@@ -4,24 +4,12 @@ import static java.lang.Math.sqrt;
 
 public class Point {
 
-  public int x1;
-  public int y1;
-  public int x2;
-  public int y2;
-
-  public Point(int x1, int x2, int y1, int y2){
-    this.x1 = x1;
-    this.x2 = x2;
-    this.y1 = y1;
-    this.y2 = y2;
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
 
   }
 
-  public Point2(int x2,int y2){
-
-  }
-
-  public double distance() {
-    return sqrt((this.x2 - this.x1)^2+(this.y2-this.y1)^2);
-  }
+  public int x;
+  public int y;
 }
