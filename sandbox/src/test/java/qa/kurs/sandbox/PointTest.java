cdd0ab856642288f7d70ac42b35277cff58fa002
Wojciech Johaniuk <wojciech.johaniuk@braintri.com>
2017-10-06 20:03:04 +0200

    fixed assertion

--- sandbox/src/test/java/qa/kurs/sandbox/PointTest.java
+++ sandbox/src/test/java/qa/kurs/sandbox/PointTest.java
@@ -7,8 +7,8 @@ public class PointTest {
 
   @Test
   public void testArea(){
-    Point p1 = new Point(2, 2);
+    Point p1 = new Point(2, 4);
     Point p2 = new Point(4, 4);
-    Assert.assertEquals( p1.distance(p1, p2), 2.0);
+    Assert.assertEquals( p1.distance(p2), 2.0);
   }
 }
