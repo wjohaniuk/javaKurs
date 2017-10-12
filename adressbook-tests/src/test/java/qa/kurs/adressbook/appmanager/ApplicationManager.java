64140fb341a80ac392176bbe5d9aaf3fe2ffa7a1
wjohaniuk <wojciech.johaniuk@gmail.com>
2017-10-12 13:14:23 +0200

    Objects equals has been changed.

--- adressbook-tests/src/test/java/qa/kurs/adressbook/appmanager/ApplicationManager.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -24,11 +25,11 @@ public class ApplicationManager {
 
     public void init() {
 
-        if (browser == BrowserType.FIREFOX) {
+        if ((browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.CHROME) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
 
