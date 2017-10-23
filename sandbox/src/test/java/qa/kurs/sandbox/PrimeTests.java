6b848b0e3a2b0b3dbc764bd7b814f324d42d8a71
Wojciech Johaniuk <wojciech.johaniuk@braintri.com>
2017-10-23 11:42:07 +0200

    isPrime Lesson

---
+++ sandbox/src/test/java/qa/kurs/sandbox/PrimeTests.java
@@ -0,0 +1,23 @@
+package qa.kurs.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+}
