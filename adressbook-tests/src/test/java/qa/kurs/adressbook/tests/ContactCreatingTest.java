b2a58b05c74ac55798079c5fc931515864c85cdd
Wojciech Johaniuk <wojciech.johaniuk@braintri.com>
2017-10-25 11:49:16 +0200

    var lambda added to CreationTests (group and contact)

--- adressbook-tests/src/test/java/qa/kurs/adressbook/tests/ContactCreatingTest.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/tests/ContactCreatingTest.java
@@ -15,8 +15,8 @@ public class ContactCreatingTest extends TestBase {
   public void testContactCreation() {
 
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("testowy", "tester",
-            "test", null, "test", "test1");
+    ContactData contact = new ContactData("testowy1", "tester",
+            "test2", null, "test", "test1");
     app.getNavigationHelper().gotoCreateContact();
     app.getContactHelper().fillContactForm(contact, true);
     app.getContactHelper().submitContact();
@@ -24,14 +24,10 @@ public class ContactCreatingTest extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
+
+
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max){
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
