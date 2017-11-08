3982e9934c3666ef2af05095243b4e9a6bb980c3
wjohaniuk <wojciech.johaniuk@gmail.com>
2017-11-08 13:10:51 +0100

    opis konfiguracji testowej

--- adressbook-tests/src/test/java/qa/kurs/adressbook/appmanager/ApplicationManager.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     private String browser;
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -17,12 +23,15 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         if ((browser.equals(BrowserType.FIREFOX))) {
             wd = new FirefoxDriver();
@@ -33,12 +42,12 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost:8080/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("secret", "admin");
+        sessionHelper.login(properties.getProperty("web.adminPass"), properties.getProperty("web.adminLogin"));
     }
 
     public void stop() {
--- adressbook-tests/src/test/java/qa/kurs/adressbook/tests/TestBase.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import qa.kurs.adressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
