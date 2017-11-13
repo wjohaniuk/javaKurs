0e00b8f1b865341bf32ffbe884e0d83f5dabcf0d
wjohaniuk <wojciech.johaniuk@gmail.com>
2017-11-13 16:25:07 +0100

    omr lekcja 2

--- adressbook-tests/src/test/java/qa/kurs/adressbook/model/GroupData.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/model/GroupData.java
@@ -3,80 +3,94 @@ package qa.kurs.adressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("group")
-
+@Entity
+@Table(name = "group_list")
 public class GroupData {
-  @XStreamOmitField
-  private int id = Integer.MAX_VALUE;
-  @Expose
-  private String name;
-  @Expose
-  private String header;
-  @Expose
-  private String footer;
+    @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
+    private int id = Integer.MAX_VALUE;
+    @Expose
+    @Column(name = "group_name")
+    private String name;
+    @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
+    private String header;
+    @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
+    private String footer;
 
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getHeader() {
-    return header;
-  }
+    public String getHeader() {
+        return header;
+    }
 
-  public String getFooter() {
-    return footer;
-  }
+    public String getFooter() {
+        return footer;
+    }
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-  }
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
-  public GroupData withId(int id) {
-    this.id = id;
-    return this;
-  }
+    public GroupData withId(int id) {
+        this.id = id;
+        return this;
+    }
 
-  public GroupData withName(String name) {
-    this.name = name;
-    return this;
-  }
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
 
-  public GroupData withHeader(String header) {
-    this.header = header;
-    return this;
-  }
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
 
-  public GroupData withFooter(String footer) {
-    this.footer = footer;
-    return this;
-  }
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-    GroupData groupData = (GroupData) o;
+        GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
 
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
 
--- adressbook-tests/src/test/java/qa/kurs/adressbook/tests/GroupCreatingTest.java
+++ adressbook-tests/src/test/java/qa/kurs/adressbook/tests/GroupCreatingTest.java
@@ -3,13 +3,14 @@ package qa.kurs.adressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import qa.kurs.adressbook.model.GroupData;
 import qa.kurs.adressbook.model.Groups;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,48 +19,52 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreatingTest extends TestBase {
+    Logger logger = LoggerFactory.getLogger(GroupCreatingTest.class);
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromJson() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")))) {
-            String json ="";
+            String json = "";
             String line = reader.readLine();
-            while (line != null){
+            while (line != null) {
                 json += line;
                 line = reader.readLine();
             }
             Gson gson = new Gson();
-            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
-            return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+            List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+            }.getType());
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
     }
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
-       try ( BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
-           String xml ="";
-           String line = reader.readLine();
-           while (line != null){
-               xml += line;
-               line = reader.readLine();
-           }
-           XStream xstream = new XStream();
-           xstream.processAnnotations(GroupData.class);
-           List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-           return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
-       }
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
+            String xml = "";
+            String line = reader.readLine();
+            while (line != null) {
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xstream = new XStream();
+            xstream.processAnnotations(GroupData.class);
+            List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+            return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+        }
     }
 
     @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
+        logger.info("Start test testGroupCreation");
         app.goTo().groupPage();
         Groups before = app.group().all();
         app.group().create(group);
         assertThat(app.group().Count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
-
-
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        logger.info("Stop test testGroupCreation");
     }
+
 }
